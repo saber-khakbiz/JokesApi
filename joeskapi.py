875b0bfd7385f21c892392eef39d2abf5092ab95
Saber <saberkhakbiz73@gmail.com>
2023-04-09 18:41:08 +0330

    add err handling to project

--- joeskapi.py
+++ joeskapi.py
@@ -1,5 +1,7 @@
+import socket
 import requests
 from abc import ABC, abstractmethod
+
 
 # Create an abstract class Joke that contains an abstract method get_random_joke
 
@@ -9,17 +11,45 @@ class Joke(ABC):
     def get_random_joke(self):
         pass
 
+
+class JokeAPIError(Exception):
+    pass
+
+
+class NoInternetConnectionError(Exception):
+    pass
+
 # Create a class JokeAPIY that also inherits from the JokeAPI abstract class
+
+
 class JokeAPIY(Joke):
+
     def get_random_joke(self):
+
+        try:
+            # Try to establish a connection to google DNS server
+            socket.create_connection(('8.8.8.8', 53), timeout=3)
+        except OSError:
+            # If a connection could not be established, raise an error
+            raise NoInternetConnectionError('No internet connection')
+
         # Send a GET request to the JokeAPIY API and get the response
         response = requests.get('https://v2.jokeapi.dev/joke/Any')
+
+        # Check if the response is successful (status code 200)
+        if response.status_code != 200:
+            raise JokeAPIError('JokeAPIY API returned an error')
+
         # Convert the response to JSON format
         json_data = response.json()
+
+        # Check if the JSON response contains the 'setup' and 'delivery' keys
+        if 'setup' not in json_data or 'delivery' not in json_data:
+            raise JokeAPIError('JokeAPIY API returned invalid data')
+
         # Extract the joke and return it
         return json_data['setup'] + '\n' + json_data['delivery']
-    
-    
+
 
 joke_api_y = JokeAPIY()
 joke1 = joke_api_y.get_random_joke()
