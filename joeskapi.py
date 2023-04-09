c32a8afb5da7f32a160503ba1e65d43651054438
Saber <saberkhakbiz73@gmail.com>
2023-04-09 18:17:47 +0330

    Update joeskapi.py
    + add JokeAPIY class to Project.

--- joeskapi.py
+++ joeskapi.py
@@ -8,3 +8,19 @@ class Joke(ABC):
     @abstractmethod
     def get_random_joke(self):
         pass
+
+# Create a class JokeAPIY that also inherits from the JokeAPI abstract class
+class JokeAPIY(Joke):
+    def get_random_joke(self):
+        # Send a GET request to the JokeAPIY API and get the response
+        response = requests.get('https://v2.jokeapi.dev/joke/Any')
+        # Convert the response to JSON format
+        json_data = response.json()
+        # Extract the joke and return it
+        return json_data['setup'] + '\n' + json_data['delivery']
+    
+    
+
+joke_api_y = JokeAPIY()
+joke1 = joke_api_y.get_random_joke()
+print(joke1)
