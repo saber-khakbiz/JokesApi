1b1b089172059616da5e06646a25700420c6fc46
Saber <saberkhakbiz73@gmail.com>
2023-04-10 18:59:10 +0330

    add jokeapininjas class to project

--- joeskapi.py
+++ joeskapi.py
@@ -1,8 +1,9 @@
 from err_handel import NoInternetConnectionError, JokeAPIError
-from cowsay import cow, get_output_string
+from cowsay import cow, pig, get_output_string
 from abc import ABC, abstractmethod
 import requests
 import socket
+import json
 
 
 # Create an abstract class Joke that contains an abstract method get_random_joke
@@ -23,8 +24,8 @@ class Joke(ABC):
     def get_random_joke(self):
         pass
 
-
-# Create a class JokeAPIY that also inherits from the JokeAPI abstract class
+#!=============================first Class=======================================
+# Create a class JokeAPIY that also inherits from the Joke abstract class
 
 
 class JokeAPIY(Joke):
@@ -50,9 +51,39 @@ class JokeAPIY(Joke):
         # Extract the joke and return it
         return json_data['setup'] + '\n' + json_data['delivery']
 
+#!++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+#!=============================Seconde Class====================================
+# Create a class JokeAPINinjas that also inherits from the Joke abstract class
+
+class JokeAPINinjas(Joke):
+
+    def get_random_joke(self, limit=1) -> str:
+        # How many results to return. Must be between 1 and 30. Default is 1.
+        self.limit = limit
+        MY_API_KEY = '08grfo6DmJVnThqWHpaZQQ==ZjzPDIMiJY0x0xmh'
+        api_url = 'https://api.api-ninjas.com/v1/jokes?limit={}'.format(
+            self.limit)
+        # Send a GET request to the JokeAPINinjas API and get the response
+        response = requests.get(api_url, headers={'X-Api-Key': MY_API_KEY})
+
+        # Check if the response is successful (status code 200)
+        if response.status_code == requests.codes.ok:
+            joke_lst = json.loads(response.text)
+            for joke in joke_lst:
+                pig(joke["joke"])
+        else:
+            print("Error:", response.status_code, response.text)
+
+#!+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
 
 if __name__ == "__main__":
 
     joke_api_y = JokeAPIY()
     joke1 = joke_api_y.get_random_joke()
     cow(joke1)
+
+    # joke_api_ninjas = JokeAPINinjas()
+    # joke2 = joke_api_ninjas.get_random_joke(limit=2)
